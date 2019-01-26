45fdb98ce261ca96ad3bbd11bd007c1320379b0b
Reddeyfish <edrichd@uci.edu>
2019-01-26 10:08:13 -0800

    Add interactable objects

---
+++ ggj2019/Assets/Scripts/Interactive/PlayerInteractor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractor : MonoBehaviour
+{
+    private Interactable target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Physics.Raycast(transform.position, transform.forward, out hitInfo, float.MaxValue, LayerMask.GetMask("Interactable"));
+        if(hitInfo.transform == null)
+        {
+            //no hit
+            target?.SetLookedAt(false);
+            target = null;
+
+            return;
+        }
+        Interactable hitTarget = hitInfo.transform.GetComponent<Interactable>();
+        if(hitTarget == null)
+        {
+            Debug.LogError("Target missing interactable");
+            return;
+        }
+        if (hitTarget != target)
+        {
+            target?.SetLookedAt(false);
+            hitTarget.SetLookedAt(true);
+            target = hitTarget;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            target.Interact();
+        }
+    }
+}
