b6371ecb283ef77a36f69fd0fd53ad6fd529e93d
Reddeyfish <edrichd@uci.edu>
2019-01-26 14:40:20 -0800

    Add toilet

---
+++ ggj2019/Assets/Interactables/Toilet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Toilet : Interactable
+{
+    AudioSource sfx;
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        base.Awake();
+        sfx = GetComponent<AudioSource>();
+    }
+
+    public override void Interact(Pickup heldObject, PlayerInteractor player)
+    {
+        base.Interact(heldObject, player);
+        sfx.Play();
+        //bathroom complete
+    }
+}
