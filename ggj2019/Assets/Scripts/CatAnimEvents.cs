ddfa5abfd160956a75030b8db088edb65327fda6
Greg Duarte <gregduarte97@gmail.com>
2019-01-27 13:54:53 -0800

    Polished and put together cat anims, jumping and all

---
+++ ggj2019/Assets/Scripts/CatAnimEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimEvents : MonoBehaviour
+{
+    public Animator anim;
+
+    public void SetInJumpLoopTrue()
+    {
+        anim.SetBool("InJumpLoop", true);
+    }
+
+    public void SetFinishJumpFlags()
+    {
+        anim.SetBool("FinishJump", false);
+        anim.SetBool("AfterJumpFinish", true);
+    }
+}
