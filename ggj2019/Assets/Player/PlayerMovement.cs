c8e2b4d5a34cb22697a5814a53c8728a8ca82cfa
Reddeyfish <edrichd@uci.edu>
2019-01-26 22:04:33 -0800

    Add footsteps

--- ggj2019/Assets/Player/PlayerMovement.cs
+++ ggj2019/Assets/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
@@ -12,7 +13,15 @@ public class PlayerMovement : MonoBehaviour
     protected float sensitivityFactor = 4f;
     private Rigidbody rigid;
     private Transform cameraHolder;
+    private AudioSource foostepSource;
     public Vector3 cameraEuler = Vector3.zero;
+    private float distanceSinceLastStep;
+    Vector3 previousPosition;
+
+    [SerializeField]
+    protected float distancePerFootstep = 1.5f;
+    [SerializeField]
+    protected AudioClip[] footsteps;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
 
         rigid = GetComponent<Rigidbody>();
         cameraHolder = GetComponentInChildren<Camera>().transform;
+        foostepSource = GetComponent<AudioSource>();
+        previousPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -38,6 +49,15 @@ public class PlayerMovement : MonoBehaviour
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
+        }
+
+        distanceSinceLastStep += (transform.position - previousPosition).magnitude;
+        previousPosition = transform.position;
+
+        if(distanceSinceLastStep > distancePerFootstep)
+        {
+            foostepSource.PlayOneShot(footsteps[Random.Range(0, footsteps.Length)]);
+            distanceSinceLastStep -= distancePerFootstep;
         }
     }
 }
