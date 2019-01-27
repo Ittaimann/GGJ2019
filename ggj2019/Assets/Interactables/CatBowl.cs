d1a067ce683ff1c9ae3cc40eebf572c619d05214
jetsontwo <jetsontwo@gmail.com>
2019-01-27 13:39:33 -0800

    Added Cat Food and Changed Player Interaction Anchor Off Of Middle Of Screen

--- ggj2019/Assets/Interactables/CatBowl.cs
+++ ggj2019/Assets/Interactables/CatBowl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CatBowl : Interactable
 {
     public GameDataScriptable gameDataScriptable;
+    public GameObject catFood;
 
     public override void StartDay()
     {
@@ -13,11 +14,17 @@ public class CatBowl : Interactable
         gameDataScriptable.foodReady = false;
     }
 
+    private void FixedUpdate()
+    {
+        catFood.SetActive(gameDataScriptable.foodReady);
+    }
+
     public override void Interact(Pickup heldObject, PlayerInteractor player)
     {
         base.Interact(heldObject, player);
         if(heldObject is CatFood)
         {
+            catFood.SetActive(true);
             gameDataScriptable.foodReady = true;
         }
     }
